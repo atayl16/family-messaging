434dd3288720c4ebbf0bcea2a753323e40e19bc6
Alisha Taylor <inspirebyexample0@gmail.com>
2025-06-22 10:57:15 +0100

    fix: robustly handle onChange for MentionsInput

--- client/src/components/ChatRoom.jsx
+++ client/src/components/ChatRoom.jsx
@@ -65,7 +65,7 @@ function ChatRoom({ room, currentUser, onSendMessage, onLeaveRoom }) {
             </button>
             <MentionsInput
               value={messageInput}
-              onChange={(event, newValue) => setMessageInput(newValue)}
+              onChange={(event, newValue) => setMessageInput(newValue || '')}
               placeholder="Type a message or @mention a user..."
               className="mentions"
               onFocus={() => setShowEmojiPicker(false)}
